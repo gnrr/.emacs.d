fbae8142f158699ed84938ed9d36454059a166a5
hidsh <hideaki.sh@gmail.com>
2018-08-05 08:28:53 +0900

    add ivy/counsel

---
+++ elpa/kivy-mode-20180702.2029/kivy-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "kivy-mode" "20180702.2029" "Emacs major mode for editing Kivy files" 'nil :commit "bf8869f84453ab5e0cc54d5f2ea0fa3a3c8a0fba" :authors '(("Dean Serenevy" . "dean@serenevy.net")) :maintainer '("Dean Serenevy" . "dean@serenevy.net"))
