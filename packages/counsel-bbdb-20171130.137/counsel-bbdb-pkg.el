66f697eab8fc6f2e82f29b632eb4f3e624b1ad58
hidsh <hideaki.sh@gmail.com>
2018-08-11 14:41:51 +0900

    fix git-gutter-fringe

---
+++ packages/counsel-bbdb-20171130.137/counsel-bbdb-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-bbdb" "20171130.137" "Quick search&input email from BBDB based on ivy" '((ivy "0.8.0") (emacs "24.3")) :commit "c86f4b9ef99c9db0b2c4196a300d61300dc2d0c1" :keywords '("mail" "abbrev" "convenience" "matching") :authors '(("Chen Bin <chenbin.sh AT gmail>")) :maintainer '("Chen Bin <chenbin.sh AT gmail>") :url "https://github.com/redguard/counsel-bbdb")
