eff46b9f1eebe68a74e0f5f188f01819430181a0
hidsh <hideaki.sh@gmail.com>
2020-12-03 14:01:40 +0900

    mod

--- init.el
+++ init.el
@@ -2222,21 +2222,19 @@ See `font-lock-add-keywords' and `font-lock-defaults'."
   ;; ----------
   (defun my-org-todo-goto-working-forward ()
     (interactive)
-    ;; fixme use cl-flet
-    (defun find () (re-search-forward  "^*+ \\[!\\] " nil 1))
-    (unless (funcall 'find)
-      (goto-char (point-min))
-      (funcall 'find)))
+    (flet ((find () (re-search-forward  "^*+ \\[!\\] " nil 1)))
+      (unless (funcall 'find)
+        (goto-char (point-min))
+        (funcall 'find))))
 
   (defun my-org-todo-goto-working-backward ()
     (interactive)
-    ;; fixme use cl-flet
-    (defun find () (prog2 (goto-char (line-beginning-position))
-                       (re-search-backward "^*+ \\[!\\] " nil 1)
-                     (goto-char (+ (point) (- (match-end 0) (match-beginning 0))))))
-    (unless (funcall 'find)
-      (goto-char (point-max))
-      (funcall 'find)))
+    (flet ((find () (prog2 (goto-char (line-beginning-position))
+                        (re-search-backward "^*+ \\[!\\] " nil 1)
+                        (goto-char (+ (point) (- (match-end 0) (match-beginning 0)))))))
+      (unless (funcall 'find)
+        (goto-char (point-max))
+        (funcall 'find))))
 
   ;; ----------
   (defun my-org-dup-heading-up ()
