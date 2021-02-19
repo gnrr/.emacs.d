5467768871fa16396874369f167d7cb3364ddf6f
hidsh <hideaki.sh@gmail.com>
2021-02-19 18:32:28 +0900

    add company-mode

--- init.el
+++ init.el
@@ -2783,6 +2783,52 @@ Thx to https://qiita.com/duloxetine/items/0adf103804b29090738a"
   (slime-setup '(slime-repl slime-fancy slime-banner))
   )
 ;; ----------------------------------------------------------------------
+(use-package company-quickhelp
+  :ensure t
+  :config
+  (setq company-quickhelp-color-background (face-background 'default))
+  (setq company-quickhelp-color-foreground (face-foreground 'default))
+  )
+;; ----------------------------------------------------------------------
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0)
+  ;; (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+
+  (defun company--insert-candidate2 (candidate)
+    (when (> (length candidate) 0)
+      (setq candidate (substring-no-properties candidate))
+      (if (eq (company-call-backend 'ignore-case) 'keep-prefix)
+          (insert (company-strip-prefix candidate))
+        (if (equal company-prefix candidate)
+            (company-select-next)
+          (delete-region (- (point) (length company-prefix)) (point))
+          (insert candidate))
+        )))
+
+  (defun company-complete-common2 ()
+    (interactive)
+    (when (company-manual-begin)
+      (if (and (not (cdr company-candidates))
+               (equal company-common (car company-candidates)))
+          (company-complete-selection)
+        (company--insert-candidate2 company-common))))
+
+  (define-key company-active-map [tab] 'company-complete-common2)
+  (define-key company-active-map [backtab] 'company-select-previous) ; おまけ
+
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-j") 'company-select-next)
+  (define-key company-active-map (kbd "C-k") 'company-select-previous)
+  (company-quickhelp-mode +1)
+  )
+;; ----------------------------------------------------------------------
 (use-package yasnippet
   :disabled
   :ensure t
